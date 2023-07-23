8f3a21ead669d8a970bed4a4eb5a621c027d0029
KoyuKansou <127967206+KoyuKansou@users.noreply.github.com>
2023-07-24 02:25:53 +0700

    Update placenl-userscript-autoupdater.user.js

--- placenl-userscript-autoupdater.user.js
+++ placenl-userscript-autoupdater.user.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name         PlaceNL Userscript (Autoupdater)
-// @namespace    https://github.com/PlaceNL/Userscript
-// @version      0.0.2
-// @description  The easiest way to run our automated placer and keep it up to date, right from your browser
-// @author       PlaceNL
+// @name         Inabacord Userscript (Autoupdater)
+// @namespace    https://github.com/KyonaKansou/Userscript
+// @version      0.7.2.7
+// @description  Inabakumori Station Userscript, forked from PlaceNL
+// @author       KyonaKansou 
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @connect      reddit.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
-// @updateURL    https://github.com/PlaceNL/Userscript/releases/download/latest/placenl-userscript-autoupdater.user.js
-// @downloadURL  https://github.com/PlaceNL/Userscript/releases/download/latest/placenl-userscript-autoupdater.user.js
+// @updateURL    https://github.com/KyonaKansou/Userscript/releases/download/latest/placenl-userscript-autoupdater.user.js
+// @downloadURL  https://github.com/KyonaKansou/Userscript/releases/download/latest/placenl-userscript-autoupdater.user.js
 // @grant        GM.xmlHttpRequest
 // @connect      github.com
 // @connect      objects.githubusercontent.com
 // ==/UserScript==
 
-const SCRIPT_LOCATION = 'https://github.com/PlaceNL/Userscript/releases/download/latest/placenl-userscript.user.js';
+const SCRIPT_LOCATION = 'https://github.com/KyonaKansou/Userscript/releases/download/latest/placenl-userscript.user.js';
 const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000;
 
 (function () {
@@ -30,7 +30,7 @@ const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000;
         url: SCRIPT_LOCATION,
         onload: (response) => {
             if (response.status < 200 || response.status > 299) {
-                alert('An error occured while loading the script. Please wait a bit and refresh the page.\n\nEr is een fout opgetreden bij het laden van het script. Wacht een ogenblikje en ververs de pagina.');
+                alert('An error occured while loading the script. Please wait a bit and refresh the page.');
                 return;
             }
             const scriptData = response.responseText;
@@ -38,7 +38,7 @@ const UPDATE_CHECK_INTERVAL = 10 * 60 * 1000;
             try {
                 eval(scriptData);
             } catch (e) {
-                alert('An error occurred while starting the script. Please wait a bit and refresh the page.\n\nEr is een fout opgetreden bij het starten van het script. Wacht een ogenblikje en ververs de pagina.');
+                alert('An error occurred while starting the script. Please wait a bit and refresh the page.');
                 return;
             }
 
